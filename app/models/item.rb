26c15d7811e1a57e986e999a91e08dab9415838c
chan_ume <dottobasic673@gmail.com>
2015-08-11 21:30:37 +0000

    want&have model complete

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,11 @@ class Item < ActiveRecord::Base
 
   has_many :ownerships  , foreign_key: "item_id" , dependent: :destroy
   has_many :users , through: :ownerships
+
+  has_many :wnats. class_name="want_users", foreign_key: "item_id", dependent: :destroy
+  has_many :want_users, through: :wants, source: :user
+  
+  has_many :haves, class_name: "Have", foreign_key: "item_id", dependent: :destroy
+  has_many :have_users , through: :haves, source: :user
+
 end
