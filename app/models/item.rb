c750f00a8d2ab409eb0863dc2dfa84ad5391a4dd
chan_ume <dottobasic673@gmail.com>
2015-08-12 10:29:38 +0000

    modify 3nd

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ActiveRecord::Base
   has_many :ownerships  , foreign_key: "item_id" , dependent: :destroy
   has_many :users , through: :ownerships
 
-  has_many :wants. class_name="want_users", foreign_key: "item_id", dependent: :destroy
+  has_many :wants. class_name="Want", foreign_key: "item_id", dependent: :destroy
   has_many :want_users, through: :wants, source: :user
   
   has_many :haves, class_name: "Have", foreign_key: "item_id", dependent: :destroy
