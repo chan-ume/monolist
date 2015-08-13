ae22ef13bece6ff2bd32f2f3f5f15a2fee2f90ef
chan_ume <dottobasic673@gmail.com>
2015-08-13 06:20:38 +0000

    want/have botton ok

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -11,8 +11,9 @@ class OwnershipsController < ApplicationController
     # itemsテーブルに存在しない場合はAmazonのデータを登録する。
     if @item.new_record?
       begin
+       #binding.pry
         # TODO 商品情報の取得 Amazon::Ecs.item_lookupを用いてください
-        response = Amazon::Ecs.item_lookup(@item.asin, :response_group => 'Medium' , :search_index => 'All' , :country => 'jp')
+        response = Amazon::Ecs.item_lookup(params[:asin], :response_group => 'Medium', :country => 'jp')
       rescue Amazon::RequestError => e
         return render :js => "alert('#{e.message}')"
       end
@@ -31,11 +32,10 @@ class OwnershipsController < ApplicationController
     # params[:type]の値ににHaveボタンが押された時にはの時は「Have」,
     # Wantボタンがされた時には「Want」が設定されています。
     
-    if type == "want"
-      current_user.want(@item)
-    
-    else
+    if params[:type] == "Have"
       current_user.have(@item)
+    else
+      current_user.want(@item)
     end    
   
   end
@@ -46,10 +46,10 @@ class OwnershipsController < ApplicationController
     # TODO 紐付けの解除。 
     # params[:type]の値ににHavedボタンが押された時にはの時は「Have」,
     # Wantedボタンがされた時には「Want」が設定されています。
-    if tyoe =="want"
-      current_user.unwant(@item)
-    else
+    if params[:type] == "Have"
       current_user.unhave(@item)
+    else
+      current_user.unwant(@item)
     end
 
   end
